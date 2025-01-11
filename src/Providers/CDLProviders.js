9cf754bde0d4b1854a270dfc8a8a6e2fe899210a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-11 14:39:18 -0500

    Add feature to pass file information to worker in CDLProvider.

--- src/Providers/CDLProviders.js
+++ src/Providers/CDLProviders.js
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 
 import PropTypes from "prop-types";
 
+import CDL_WORKER_PROTOCOL from "../Services/CDL_WORKER_PROTOCOL";
 import AppStateContext from "./AppStateContext";
 
 CDLProviders.propTypes = {
@@ -18,11 +19,42 @@ CDLProviders.propTypes = {
 function CDLProviders ({children, filePath}) {
     const [appState, setAppState] = useState();
 
+    const cdlWorker = useRef(null);
+
+    // Terminate worker when component is destroyed.
+    useEffect(() => {
+        return () => {
+            if (cdlWorker.current) {
+                cdlWorker.current.terminate();
+            }
+        };
+    }, []);
+
+    // Create worker to handle file.
     useEffect(() => {
         if (filePath) {
-            console.info("File Path:", filePath);
+            if (cdlWorker.current) {
+                cdlWorker.current.terminate();
+            }
+            cdlWorker.current = new Worker(new URL("../Services/cdlWorker.js", import.meta.url));
+            cdlWorker.current.onmessage = handleWorkerMessage;
+            cdlWorker.current.postMessage({
+                code: CDL_WORKER_PROTOCOL.LOAD_FILE,
+                fileInfo: filePath,
+            });
         }
     }, [filePath]);
+
+    /**
+     * Handles message from the worker.
+     * @param {object} event
+     */
+    const handleWorkerMessage = useCallback((event) => {
+        switch (event.data.code) {
+            default:
+                break;
+        }
+    });
 
     return (
         <AppStateContext.Provider value={{appState, setAppState}}>
