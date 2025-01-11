f397ab1414177035ec45f6a77c22fa0a80e50938
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-11 16:55:40 -0500

    Update import paths in cdlWorker.js.

--- src/Services/cdlWorker.js
+++ src/Services/cdlWorker.js
@@ -1,6 +1,6 @@
 
 import CDL_WORKER_PROTOCOL from "./CDL_WORKER_PROTOCOL";
-import Debugger from "./Debugger";
+import Debugger from "./classes/Debugger";
 
 let debuggerInstance;
 onmessage = function (e) {
