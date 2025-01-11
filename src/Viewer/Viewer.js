f704e45cabba1ad0296f38752a7c638d0b7ce8d5
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-10 22:38:03 -0500

    Add container layout to viewer.

--- src/Viewer/Viewer.js
+++ src/Viewer/Viewer.js
@@ -8,7 +8,21 @@ import "./Viewer.scss";
  */
 export function Viewer () {
     return (
-        <div className="viewer">
+        <div className="viewer-container">
+            <div className="menu-container">
+
+            </div>
+            <div className="body-container">
+                <div className="d-flex flex-row h-100">
+
+                </div>
+                <div className="d-flex flex-grow-1 h-100">
+
+                </div>
+            </div>
+            <div className="status-bar-container">
+
+            </div>
         </div>
     );
 }
