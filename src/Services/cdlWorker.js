ab874d534a73fa6c11df9909be99f882db1fb7d9
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-11 16:30:34 -0500

    Create a debugger class in cdlWorker when file object/URL is received.

--- src/Services/cdlWorker.js
+++ src/Services/cdlWorker.js
@@ -1,11 +1,13 @@
 
 import CDL_WORKER_PROTOCOL from "./CDL_WORKER_PROTOCOL";
+import Debugger from "./Debugger";
 
+let debuggerInstance;
 onmessage = function (e) {
     switch (e.data.code) {
         case CDL_WORKER_PROTOCOL.LOAD_FILE:
             try {
-                console.log(e.data);
+                debuggerInstance = new Debugger(e.data.fileInfo);
             } catch (e) {
                 console.log(e);
             }
