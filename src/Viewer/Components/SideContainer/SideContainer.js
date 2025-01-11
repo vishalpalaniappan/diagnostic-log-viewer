7bb20603a8f8656c8cc99b7832504cc2251b9112
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-10 22:54:38 -0500

    Add SideContainer component.

---
+++ src/Viewer/Components/SideContainer/SideContainer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./SideContainer.scss";
+
+/**
+ * Contains the side menu and accordian containers.
+ * @return {JSX.Element}
+ */
+export function SideContainer () {
+    return (
+        <div className="side-container d-flex flex-row">
+            <div className="menu">
+
+            </div>
+            <div className="accordian">
+
+            </div>
+        </div>
+    );
+}
