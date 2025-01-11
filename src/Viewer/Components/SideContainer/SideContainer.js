462103a8c6bc8fcb2242aa1d060b1bb4dfe74183
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-10 23:15:12 -0500

    Add resizable handle to side container.

--- src/Viewer/Components/SideContainer/SideContainer.js
+++ src/Viewer/Components/SideContainer/SideContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 
 import "./SideContainer.scss";
 
@@ -7,12 +7,51 @@ import "./SideContainer.scss";
  * @return {JSX.Element}
  */
 export function SideContainer () {
+    const accordian = useRef();
+    const handle = useRef();
+    const MIN_ACCORDIAN_WIDTH = 200;
+    const MAX_ACCORDIAN_WIDTH = 200;
+
+    let downValueX;
+
+    const handleMouseDown = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+        handle.current.classList.add("handle-active");
+        downValueX = e.clientX;
+    };
+
+    const handleMouseMove = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const delta = e.clientX - downValueX;
+        const newWidth = accordian.current.getBoundingClientRect().width + delta;
+        const maxWidth = document.body.clientWidth - MAX_ACCORDIAN_WIDTH;
+        if (newWidth > MIN_ACCORDIAN_WIDTH && newWidth < maxWidth) {
+            accordian.current.style.width = newWidth + "px";
+            downValueX = e.clientX;
+        }
+    };
+
+    const handleMouseUp = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", handleMouseUp);
+        handle.current.classList.remove("handle-active");
+    };
+
     return (
         <div className="side-container d-flex flex-row">
             <div className="menu">
 
             </div>
-            <div className="accordian">
+            <div ref={accordian} className="accordian">
+
+            </div>
+            <div ref={handle} onMouseDown={handleMouseDown} className="handle">
 
             </div>
         </div>
