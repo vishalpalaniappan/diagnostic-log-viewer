6694a1192f6f21c13daa33b560a7dcca48cc18d5
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-10 19:25:03 -0500

    Add webpack configuration and boilerplate React application.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Diagnostic Log Viewer
+A tool to view and interact with Compressed Diagnostic Log (CDL) files.
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+
+/**
+ * Renders the application.
+ *
+ * @return {JSX.Element}
+ */
+export function App () {
+    return (
+        <div id="app">
+        </div>
+    );
+}
