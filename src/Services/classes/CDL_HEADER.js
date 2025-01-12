a52ffec8c3d749f78f85f460b6a1295b21f7f1c1
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-11 23:31:05 -0500

    Create object which has the log types grouped by function.

--- src/Services/classes/CDL_HEADER.js
+++ src/Services/classes/CDL_HEADER.js
@@ -13,16 +13,29 @@ class CDL_HEADER {
         this.fileTree = header;
 
         this.logTypeMap = {};
+        this.functions = {};
         this.extractLogTypeMap();
     }
 
     /**
      * Extracts the logtype map for this program by processing the
-     * sst of each file in the file tree.
+     * sst of each file in the file tree. The logtype map is then
+     * processed to create an object that groups functions by logtype.
      */
     extractLogTypeMap () {
         Object.keys(this.fileTree).forEach((fileName, index) => {
             this.processSST(this.fileTree[fileName].sst, "global");
+        });
+
+        // Note that this information can be extracted in the previous
+        // step. It is separated here because it improves readability.
+        Object.keys(this.logTypeMap).forEach((ltIndex, index) => {
+            const ltMap = this.logTypeMap[ltIndex];
+            if (ltMap.fid in this.functions) {
+                this.functions[ltMap.fid].push(ltMap.id);
+            } else {
+                this.functions[ltMap.fid] = [ltMap.id];
+            }
         });
     }
 
