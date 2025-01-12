090f35486b42fdefd082a7d86088675ae57caef7
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-11 21:47:43 -0500

    Add feature to extract the log type map to the CDL Header class.

--- src/Services/classes/CDL_HEADER.js
+++ src/Services/classes/CDL_HEADER.js
@@ -11,6 +11,35 @@ class CDL_HEADER {
     constructor (IRStreamHeader) {
         const header = JSON5.parse(IRStreamHeader);
         this.fileTree = header;
+
+        this.logTypeMap = {};
+        this.extractLogTypeMap();
+    }
+
+    /**
+     * Extracts the logtype map for this program by processing the
+     * sst of each file in the file tree.
+     */
+    extractLogTypeMap () {
+        Object.keys(this.fileTree).forEach((fileName, index) => {
+            this.processSST(this.fileTree[fileName].sst);
+        });
+    }
+
+
+    /**
+     * This function recursively processes the SST.
+     * @param {Object} root
+     */
+    processSST (root) {
+        const nodes = root.children.concat(root.siblings);
+        nodes.forEach((child, index) => {
+            this.logTypeMap[child.id] = child;
+
+            if (child.type === "function" || child.type === "root") {
+                this.processSST(child);
+            }
+        });
     }
 
     /**
