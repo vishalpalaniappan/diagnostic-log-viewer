dd117627bbf8b09b30fee75badb867d912320d2c
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-13 18:56:07 -0500

    Remove deleted file from imports.

--- src/Providers/CDLProviders.js
+++ src/Providers/CDLProviders.js
@@ -5,7 +5,6 @@ import PropTypes from "prop-types";
 import CDL_WORKER_PROTOCOL from "../Services/CDL_WORKER_PROTOCOL";
 import AppStateContext from "./AppStateContext";
 import FileTreeContext from "./FileTreeContext";
-import PositionContext from "./PositionMetadataContext";
 
 CDLProviders.propTypes = {
     children: PropTypes.object,
