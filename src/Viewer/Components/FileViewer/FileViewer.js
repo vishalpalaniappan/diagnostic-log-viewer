56df96afdac80d3f78b6aeda009913988473cc57
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-12 20:31:57 -0500

    Consume fileTree context in fileViewer.

--- src/Viewer/Components/FileViewer/FileViewer.js
+++ src/Viewer/Components/FileViewer/FileViewer.js
@@ -1,7 +1,9 @@
-import React, {useRef} from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 
 import Editor, {loader} from "@monaco-editor/react";
 import * as monaco from "monaco-editor";
+
+import FileTreeContext from "../../../Providers/FileTreeContext";
 
 import "./FileViewer.scss";
 import "monaco-editor/min/vs/editor/editor.main.css";
@@ -11,6 +13,9 @@ import "monaco-editor/min/vs/editor/editor.main.css";
  * @return {JSX.Element}
  */
 export function FileViewer () {
+    const {fileTree} = useContext(FileTreeContext);
+    const [files, setFiles] = useState();
+
     const editorRef = useRef(null);
     const monacoRef = useRef(null);
     loader.config({monaco});
@@ -32,11 +37,16 @@ export function FileViewer () {
         editorRef.current = editor;
     };
 
+    useEffect(() => {
+        if (fileTree) {
+            setFiles(Object.keys(fileTree));
+        }
+    }, [fileTree]);
+
 
     return (
         <div className="file-view-container d-flex flex-column">
             <div className="tabs d-flex">
-
             </div>
             <div className="editor d-flex flex-grow-1">
                 <Editor
