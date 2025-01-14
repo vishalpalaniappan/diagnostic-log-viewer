1ea4d11a710b1736a54346d03f9c3da14102798a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-13 20:46:27 -0500

    Add CallStack container component.

---
+++ src/Viewer/Components/SideContainer/DebugContainer/CallStackContainer/CallStackContainer.js
@@ -0,0 +1,28 @@
+import React, {useContext, useEffect, useState} from "react";
+
+import AppStateContext from "../../../../../Providers/AppStateContext";
+
+import "./CallStackContainer.scss";
+
+/**
+ * Contains the call stack container.
+ * @return {JSX.Element}
+ */
+export function CallStackContainer () {
+    const [callStack, setCallStack] = useState();
+
+    const {appState} = useContext(AppStateContext);
+
+    useEffect(() => {
+        if (appState) {
+            console.log(appState.callStack);
+            setCallStack(appState.callStack);
+        }
+    }, [appState]);
+
+    return (
+        <div className="callStackContainer">
+            {callStack}
+        </div>
+    );
+}
