b45bfb3b4790ed793ab24c4ba53d694dae3f769e
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-13 20:23:25 -0500

    Refactor DebugContainer and add VariableStackContainer to it..

--- src/Viewer/Components/SideContainer/DebugContainer/DebugContainer.js
+++ src/Viewer/Components/SideContainer/DebugContainer/DebugContainer.js
@@ -1,6 +1,7 @@
 import React, {useRef} from "react";
 
 import {VerticleHandle} from "../VerticleHandle/VerticleHandle";
+import {VariableStackContainer} from "./VariableStackContainer/VariableStackContainer";
 
 import "./DebugContainer.scss";
 
@@ -9,21 +10,21 @@ import "./DebugContainer.scss";
  * @return {JSX.Element}
  */
 export function DebugContainer () {
-    const variableStack = useRef();
-    const callStack = useRef();
-    const breakPoints = useRef();
+    const variableStackRef = useRef();
+    const callStackRef = useRef();
+    const breakPointsRef = useRef();
 
     return (
         <div className="debug-container w-100 d-flex flex-column">
-            <div className="section" ref={variableStack}>
+            <div className="section" ref={variableStackRef}>
+                <VariableStackContainer />
+            </div>
+            <VerticleHandle topDiv={variableStackRef} bottomDiv={callStackRef}/>
+            <div className="section" ref={callStackRef}>
 
             </div>
-            <VerticleHandle topDiv={variableStack} bottomDiv={callStack}/>
-            <div className="section" ref={callStack}>
-
-            </div>
-            <VerticleHandle topDiv={callStack} bottomDiv={breakPoints}/>
-            <div className="section" ref={breakPoints}>
+            <VerticleHandle topDiv={callStackRef} bottomDiv={breakPointsRef}/>
+            <div className="section" ref={breakPointsRef}>
 
             </div>
         </div>
