629d2ee2bf58c352bbf7aa5fcb7039cc2e454c2c
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-13 20:47:20 -0500

    Refactor DebugContainer and auomatically set container size on page load.

--- src/Viewer/Components/SideContainer/DebugContainer/DebugContainer.js
+++ src/Viewer/Components/SideContainer/DebugContainer/DebugContainer.js
@@ -1,6 +1,7 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 
 import {VerticleHandle} from "../VerticleHandle/VerticleHandle";
+import {CallStackContainer} from "./CallStackContainer/CallStackContainer";
 import {VariableStackContainer} from "./VariableStackContainer/VariableStackContainer";
 
 import "./DebugContainer.scss";
@@ -10,18 +11,31 @@ import "./DebugContainer.scss";
  * @return {JSX.Element}
  */
 export function DebugContainer () {
+    const debugContainerRef = useRef();
     const variableStackRef = useRef();
     const callStackRef = useRef();
     const breakPointsRef = useRef();
 
+    const redrawContainers = () => {
+        const height = debugContainerRef.current.clientHeight;
+        const containerHeight = Math.floor(height/3) + "px";
+        callStackRef.current.style.height = containerHeight;
+        variableStackRef.current.style.height = containerHeight;
+        breakPointsRef.current.style.height = containerHeight;
+    };
+
+    useEffect(() => {
+        redrawContainers();
+    }, []);
+
     return (
-        <div className="debug-container w-100 d-flex flex-column">
+        <div ref={debugContainerRef} className="debug-container w-100 d-flex flex-column">
             <div className="section" ref={variableStackRef}>
                 <VariableStackContainer />
             </div>
             <VerticleHandle topDiv={variableStackRef} bottomDiv={callStackRef}/>
             <div className="section" ref={callStackRef}>
-
+                <CallStackContainer />
             </div>
             <VerticleHandle topDiv={callStackRef} bottomDiv={breakPointsRef}/>
             <div className="section" ref={breakPointsRef}>
