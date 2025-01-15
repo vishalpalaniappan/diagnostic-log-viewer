346305a084336d3a7a9aa5b401d6b9a3be72b46f
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-14 22:33:22 -0500

    Remove log statement.

--- src/Services/classes/CDL.js
+++ src/Services/classes/CDL.js
@@ -85,7 +85,6 @@ class CDL {
      * @return {Object}
      */
     getCallStack (position) {
-        console.log(this.callStacks);
         const cs = this.callStacks[position];
 
         const csInfo = [];
