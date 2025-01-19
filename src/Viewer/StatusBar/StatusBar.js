2626e9cf865bd016ffab83b4ca6bd9ceb5da2891
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-19 00:00:46 -0500

    Add Status Bar component.

---
+++ src/Viewer/StatusBar/StatusBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./StatusBar.scss";
+
+/**
+ * Renders the status bar.
+ * @return {JSX.Element}
+ */
+export function StatusBar () {
+    return (
+        <></>
+    );
+}
