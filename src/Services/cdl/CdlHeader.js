1fcc532710729c40e4b7d2b6d819dac6cee4de95
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-02-04 15:34:57 -0500

    Refactor cdl header and add additional checks for invalid parameters.

--- src/Services/cdl/CdlHeader.js
+++ src/Services/cdl/CdlHeader.js
@@ -13,32 +13,32 @@ class CdlHeader {
      */
     constructor (IRStreamHeader) {
         this.header = JSON5.parse(IRStreamHeader);
-        this.fileTree = this.header.fileTree;
         this.logTypeMap = {};
         this.variableMap = {};
-
         this.functionMap = {};
         this.parseHeader();
     }
 
     /**
-     * Prase the header of the CDL file and extract the following:
-     * - LogTypeMap
-     * - FunctionMap
-     * - VariableMap
+     * Prase the header of the CDL file and extract the
+     * logtype map, function map and variable map.
      */
     parseHeader () {
         Object.keys(this.header.ltMap).forEach((logtype, index) => {
-            const fileName = this._getFileFromLogType(logtype);
             const ltInfo = this.header.ltMap[logtype];
-            this.logTypeMap[logtype] = new LtInfo(ltInfo, ltInfo.funcid, fileName);
 
-            if (ltInfo.funcid in this.functionMap) {
-                this.functionMap[ltInfo.funcid].push(logtype);
-            } else {
-                this.functionMap[ltInfo.funcid] = [logtype];
-            }
+            // Add to logtype map
+            this.logTypeMap[logtype] = new LtInfo(
+                ltInfo,
+                ltInfo.funcid,
+                this._getFileFromLogType(logtype)
+            );
 
+            // Add logtype to function map
+            !(ltInfo.funcid in this.functionMap) && (this.functionMap[ltInfo.funcid] = []);
+            this.functionMap[ltInfo.funcid].push(logtype);
+
+            // Add variables to variable map
             ltInfo.vars.forEach((varInfo, index) => {
                 this.variableMap[varInfo.varId] = {
                     name: varInfo.name,
@@ -56,24 +56,28 @@ class CdlHeader {
      */
     _getFileFromLogType (logtype) {
         for (const fileName in this.header.fileTree) {
-            if (fileName) {
-                const minLt = this.fileTree[fileName].minLt;
-                const maxLt = this.fileTree[fileName].maxLt;
-                if (minLt <= logtype && maxLt >= logtype) {
-                    return fileName;
-                }
+            const minLt = this.header.fileTree[fileName].minLt;
+            const maxLt = this.header.fileTree[fileName].maxLt;
+            if (minLt <= logtype && maxLt >= logtype) {
+                return fileName;
             }
         }
+        return null;
     }
 
     /**
      * Returns the source of each file in the header.
+     *
      * @return {object} sourceTree
      */
     getSourceFiles () {
         const sourceTree = {};
         Object.keys(this.header.fileTree).forEach((fileName, index) => {
-            sourceTree[fileName] = this.header.fileTree[fileName].source;
+            const file = this.header.fileTree[fileName];
+            if (!file.source) {
+                throw new Error(`Source missing for file: ${fileName}`);
+            }
+            sourceTree[fileName] = file.source;
         });
         return sourceTree;
     }
