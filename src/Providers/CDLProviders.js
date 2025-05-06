63918702e368e81b37dbf038378fb25398264de1
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-06 17:41:28 -0400

    Remove unncessary undefined proptype.

--- src/Providers/CDLProviders.js
+++ src/Providers/CDLProviders.js
@@ -15,14 +15,14 @@ import WorkerContext from "./WorkerContext";
 CDLProviders.propTypes = {
     children: PropTypes.object,
     fileInfo: PropTypes.string,
-    executionIndex: PropTypes.oneOfType([PropTypes.number, PropTypes.oneOf([undefined])]),
+    executionIndex: PropTypes.number,
 };
 
 /**
  * Provides all contexts consumed by the application.
  * @param {JSX} children
  * @param {String} fileInfo
- * @param {Number|undefined} executionIndex
+ * @param {Number} executionIndex
  * @return {JSX}
  */
 function CDLProviders ({children, fileInfo, executionIndex}) {
