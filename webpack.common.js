c3965281a0a21df4350f4fe5a578434e1b2ea051
Vishal Palaniappan <101469936+vishalpalaniappan@users.noreply.github.com>
2025-05-08 23:19:40 -0400

    chore: add package-lock.json. (#47)

--- webpack.common.js
+++ webpack.common.js
@@ -25,17 +25,6 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 include: path.resolve(__dirname, "src"),
-                exclude: /(node_modules|bower_components|build)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/" +
-                        "preset-env", "@babel/preset-react"],
-                    },
-                },
-            },
-            {
-                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
