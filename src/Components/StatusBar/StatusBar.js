d903e8c5045c5c56abb2e30fef8b52cf2fc54183
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-26 03:29:31 -0400

    Add threads selector to status bar.

--- src/Components/StatusBar/StatusBar.js
+++ src/Components/StatusBar/StatusBar.js
@@ -1,5 +1,9 @@
 import React, {useContext, useEffect, useState} from "react";
 
+import {Diagram2Fill} from "react-bootstrap-icons";
+
+import {StatusBarMenu} from "./StatusBarMenu/StatusBarMenu";
+
 import "./StatusBar.scss";
 
 StatusBar.propTypes = {
@@ -13,8 +17,13 @@ export function StatusBar ({}) {
     return (
         <div id="status-bar">
             <div className="status-bar">
-                <div className="status-left"></div>
-                <div className="status-right "></div>
+                <div className="status-left">
+                </div>
+                <div className="status-right ">
+                    <StatusBarMenu className="status-item status-item-button status-verbosity-accent">
+                        <Diagram2Fill/><span className="ms-2 me-3">Main Thread</span>
+                    </StatusBarMenu>
+                </div>
             </div>
         </div>
     );
