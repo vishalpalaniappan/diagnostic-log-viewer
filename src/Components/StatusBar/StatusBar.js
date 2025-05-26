6f465cf65f9d7f0a338fb2feed2075976ed5daf5
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-05-26 03:08:24 -0400

    Add status bar component to allow user to select between threads.

---
+++ src/Components/StatusBar/StatusBar.js
@@ -0,0 +1,21 @@
+import React, {useContext, useEffect, useState} from "react";
+
+import "./StatusBar.scss";
+
+StatusBar.propTypes = {
+};
+
+/**
+ * Status bar of the viewer component.
+ * @return {JSX.Element}
+ */
+export function StatusBar ({}) {
+    return (
+        <div id="status-bar">
+            <div className="status-bar">
+                <div className="status-left"></div>
+                <div className="status-right "></div>
+            </div>
+        </div>
+    );
+}
