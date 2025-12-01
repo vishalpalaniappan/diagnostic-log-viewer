661b3e5ee1ab9a331ef1fa39dd78338d10122280
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-12-01 08:07:23 -0500

    Assemble tree using metadata.

--- src/Services/cdl/AbstractionMap.js
+++ src/Services/cdl/AbstractionMap.js
@@ -22,21 +22,43 @@ class AbstractionMap {
         if (this.abstractionStack.length === 0 || id in this.map) {
             for (const entry in this.map) {
                 if (entry === id) {
-                    console.log(this.map[entry].intent);
+                    console.log("FUNCTION CALL");
                     this.abstractionStack.push(this.map[entry]);
                     this.currentAbstraction = this.map[entry];
+                    console.log(this.abstractionStack.length + "," + id + ":" + this.map[entry].intent);
                     return;
                 }
             }
         }
 
         if (this.abstractionStack.length > 0) {
-            if ("abstractions" in this.currentAbstraction) {
+            do {
+                let found = false;
+                this.currentAbstraction.abstractions.forEach((abs, index) => {
+                    if (abs.id === id) {
+                        found = true;
+                    }
+                });
+
+                if (found) {
+                    break;
+                } else {
+                    this.abstractionStack.pop();
+                    const stackSize = this.abstractionStack.length - 1;
+                    this.currentAbstraction = this.abstractionStack[stackSize];
+                }
+            } while (this.abstractionStack.length > 0);
+
+            if (this.currentAbstraction && "abstractions" in this.currentAbstraction) {
                 this.currentAbstraction.abstractions.forEach((entry, index) => {
                     if (entry.id === id) {
-                        console.log(entry.intent);
-                        this.abstractionStack.push(entry);
-                        this.currentAbstraction = entry;
+                        if ("abstractions" in entry) {
+                            console.log(this.abstractionStack.length + "," + id + ":" + entry.intent);
+                            this.abstractionStack.push(entry);
+                            this.currentAbstraction = entry;
+                        } else {
+                            console.log(this.abstractionStack.length + "," + id + ":" + entry.intent);
+                        }
                         return;
                     }
                 });
