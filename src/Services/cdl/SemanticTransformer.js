e3a15540bf1a8f903a102c01ae7709666ef07bf3
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-12-26 14:21:09 -0500

    Add SemanticTransformer class.

---
+++ src/Services/cdl/SemanticTransformer.js
@@ -0,0 +1,17 @@
+
+/**
+ * This class is responsible for performing the semantic transformation
+ * on the threads to observe the behavior of the design.
+ */
+class SemanticTransformer {
+    /**
+     * Initializes the semantic transformer.
+     * @param {Object} designAbstractions
+     * @param {Object} threads
+     */
+    constructor (designAbstractions, threads) {
+
+    }
+}
+
+export default SemanticTransformer;
