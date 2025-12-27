4da56c2e7ba17315abe55202e0929b13c06243e7
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-12-27 04:14:37 -0500

    Remove unused functions from header.

--- src/Services/cdl/CdlHeader.js
+++ src/Services/cdl/CdlHeader.js
@@ -125,18 +125,7 @@ class CdlHeader {
      * @return {Boolean} Indicates if the header has an abstraction map.
      */
     hasAbstractionMap () {
-        return "sdg" in this.header;
-    }
-
-    /**
-     * @return {Object} Indicates if the header has an abstraction map.
-     */
-    getAbstractionMap () {
-        if (this.hasAbstractionMap()) {
-            return this.header["sdg"];
-        } else {
-            return null;
-        }
+        return "sdg_meta" in this.header;
     }
 
     /**
@@ -144,18 +133,6 @@ class CdlHeader {
      * or null if not present.
      */
     getSDG () {
-        if ("sdg" in this.header) {
-            return this.header["sdg"];
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * @return {Object|null} Returns the SDG metadata from the header,
-     * or null if not present.
-     */
-    getSDGMeta () {
         if ("sdg_meta" in this.header) {
             return this.header["sdg_meta"];
         } else {
