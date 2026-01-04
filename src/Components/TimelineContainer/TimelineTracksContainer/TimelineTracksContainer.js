3889ee7b53fdf8dee846f4783979450d58cb96c7
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-04 16:13:37 -0500

    Setup needle to align to right time.

--- src/Components/TimelineContainer/TimelineTracksContainer/TimelineTracksContainer.js
+++ src/Components/TimelineContainer/TimelineTracksContainer/TimelineTracksContainer.js
@@ -19,21 +19,40 @@ export function TimelineTracksContainer () {
 
     useEffect(() => {
         if (durations) {
-            timelineContainerRef.current.style.width = durations.max * PX_PER_SECOND + "px";
-            timeTrackRef.current.style.width = durations.max * PX_PER_SECOND + "px";
+            const roundedDuration = Math.ceil(durations.max);
+            timelineContainerRef.current.style.width = roundedDuration * PX_PER_SECOND + "px";
+            timeTrackRef.current.style.width = roundedDuration * PX_PER_SECOND + "px";
 
-            const width = 1 * PX_PER_SECOND;
-            needleRef.current.style.left = width + "px";
+            const left = 1 * PX_PER_SECOND + 15;
+            needleRef.current.style.left = left + "px";
 
             const fullHeight = tracksContainerRef.current.getBoundingClientRect().height - 10;
             needleRef.current.style.height = fullHeight + "px";
         }
     }, [durations]);
 
+    const getTimeTracks = () => {
+        if (durations) {
+            const ticks = [];
+            for (let i = 0; i < Math.ceil(durations.max); i++) {
+                const left = (PX_PER_SECOND * i) + "px";
+                ticks.push(
+                    <span className="trackLabel"
+                        key={i}
+                        style={{"left": left}}>
+                        {i}
+                    </span>
+                );
+            }
+            return ticks;
+        }
+    };
+
     return (
         <div ref={tracksContainerRef} className="timelineTracksContainer d-flex flex-column">
+            <div ref={needleRef} className="needle"></div>
             <div className="track-time-row" ref={timeTrackRef}>
-                <div ref={needleRef} className="needle"></div>
+                {getTimeTracks()}
             </div>
             <div className="timeline" ref={timelineContainerRef}>
             </div>
