df9fbd0c29b44046668d2e8b444e34a967c6b021
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-04 14:34:32 -0500

    Save duration of each thread and duration of program.

--- src/Components/TimelineContainer/TimelineContainer.js
+++ src/Components/TimelineContainer/TimelineContainer.js
@@ -1,18 +1,21 @@
 import React, {useCallback, useContext, useEffect, useRef, useState} from "react";
 
+import SegContext from "../../Providers/SegContext";
 import TimelineContainerContext from "./TimelineContainerContext";
+import TimelineLabelsContainer from "./TimelineLabelsContainer/TimelineLabelsContainer";
 
 import "./TimelineContainer.scss";
-import TimelineLabelsContainer from "./TimelineLabelsContainer/TimelineLabelsContainer";
 
 /**
  * Contains the timeline container.
  * @return {JSX.Element}
  */
 export function TimelineContainer () {
+    const {seg} = useContext(SegContext);
     const accordian = useRef();
     const handle = useRef();
     const downValueX = useRef();
+    const [durations, setDurations] = useState();
 
     const ACCORDIAN_WIDTH = 200;
     const MIN_EDITOR_WIDTH = 200;
@@ -56,6 +59,42 @@ export function TimelineContainer () {
             document.removeEventListener("mouseup", handleMouseUp);
         };
     }, [handleMouseMove, handleMouseUp]);
+
+
+    useEffect(() => {
+        if (seg) {
+            const threads = Object.keys(seg);
+
+            const threadDurations = {};
+            let maxDuration = 0;
+
+            // Calculate the duration of threads and save max duration
+            for (let i = 0; i < threads.length; i++) {
+                const thread = seg[threads[i]];
+                const startTimeDate = new Date(thread[0].abstraction.timestamp.unix_millisecs);
+                const endTimeDate = new Date(
+                    thread[thread.length - 1].abstraction.timestamp.unix_millisecs
+                );
+                const seconds = (endTimeDate.getTime() - startTimeDate.getTime()) / 1000;
+                threadDurations[thread] = {"duration": seconds};
+
+                if (seconds > maxDuration) {
+                    maxDuration = seconds;
+                }
+            }
+
+            setDurations({
+                "max": maxDuration,
+                "threads": threadDurations,
+            });
+        }
+    }, [seg]);
+
+    useEffect(() => {
+        if (durations) {
+            console.log(durations);
+        }
+    }, [durations]);
 
     return (
         <TimelineContainerContext.Provider value={{}}>
