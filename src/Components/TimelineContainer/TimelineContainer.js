9dd6a1e817266027a27df85e6be7d0f467a290cf
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-04 03:38:27 -0500

    Setup left and right side of timeline containers.

--- src/Components/TimelineContainer/TimelineContainer.js
+++ src/Components/TimelineContainer/TimelineContainer.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useCallback, useContext, useEffect, useRef, useState} from "react";
 
 import TimelineContainerContext from "./TimelineContainerContext";
 
@@ -9,18 +9,60 @@ import "./TimelineContainer.scss";
  * @return {JSX.Element}
  */
 export function TimelineContainer () {
+    const accordian = useRef();
+    const handle = useRef();
+    const downValueX = useRef();
+
+    const ACCORDIAN_WIDTH = 200;
+    const MIN_EDITOR_WIDTH = 200;
+    const MIN_ACCORDIAN_WIDTH = 50;
+
+    const handleMouseDown = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+        handle.current.classList.add("handle-active");
+        downValueX.current = e.clientX;
+    };
+
+    const handleMouseMove = useCallback((e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const delta = e.clientX - downValueX.current;
+        const newWidth = accordian.current.getBoundingClientRect().width + delta;
+        const MAX_ACCORDIAN_WIDTH = document.body.clientWidth - MIN_EDITOR_WIDTH;
+        if (newWidth > MIN_ACCORDIAN_WIDTH && newWidth < MAX_ACCORDIAN_WIDTH) {
+            accordian.current.style.width = newWidth + "px";
+            downValueX.current = e.clientX;
+        }
+    }, []);
+
+    const handleMouseUp = useCallback(
+        (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+            handle.current.classList.remove("handle-active");
+        },
+        [handleMouseMove]
+    );
+
+    useEffect(() => {
+        return () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+        };
+    }, [handleMouseMove, handleMouseUp]);
+
     return (
         <TimelineContainerContext.Provider value={{}}>
-            <div className="w-100 h-100 d-flex flex-row">
-                <div style={{width: "300px"}} className="">
-
-                </div>
-                <div style={{width: "1px", backgroundColor: "red"}} className="">
-
-                </div>
-                <div className="flex-grow-1">
-                    
-                </div>
+            <div className="treeMenuContainer d-flex flex-row">
+                <div style={{width: ACCORDIAN_WIDTH+"px"}}
+                    ref={accordian} className="accordian"></div>
+                <div className="handle" ref={handle} onMouseDown={handleMouseDown} />
+                <div className="flex-grow-1"></div>
             </div>
         </TimelineContainerContext.Provider>
     );
