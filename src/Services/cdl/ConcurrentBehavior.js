15850951b215c4e15089033a5ef79ac584743d65
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-09 13:38:53 -0500

    Create concurrent behavior class.

---
+++ src/Services/cdl/ConcurrentBehavior.js
@@ -0,0 +1,17 @@
+/**
+ * This class is a representation of the concurrent behavioral
+ * metadata. It will be used to move forward in the concurrent
+ * abstraction as the forks are assembled. It will essentially
+ * hold the state of the concurrent behavior as we step
+ * through the behavior of the program.
+ */
+class ConcurrenBehavior {
+    /**
+     * Initializes the behavioral meta.
+     */
+    constructor () {
+
+    }
+};
+
+export default ConcurrenBehavior;
