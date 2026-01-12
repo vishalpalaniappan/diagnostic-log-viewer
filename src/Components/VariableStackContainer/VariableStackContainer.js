f669e317b6553c8394c3c3f90211ea15e7a0c4cc
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-12 06:49:16 -0500

    Change color of line in variable stack.

--- src/Components/VariableStackContainer/VariableStackContainer.js
+++ src/Components/VariableStackContainer/VariableStackContainer.js
@@ -29,7 +29,7 @@ export function VariableStackContainer () {
     const variableStackTheme = {
         base00: "#1e1e1e",
         base01: "#ddd",
-        base02: "#474747",
+        base02: "#2c2c2c",
         base03: "#444",
         base04: "#717171",
         base05: "#444",
