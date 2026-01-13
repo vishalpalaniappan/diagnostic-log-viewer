4d987e6df1a9cdec4ffcfe1946de810c32c24668
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-13 18:42:53 -0500

    Add sequential design abstraction class.

---
+++ src/Services/cdl/SequentialDesignAbstraction.js
@@ -0,0 +1,16 @@
+/**
+ * This class holds the sequential design abstraction
+ * and processes it using the metadata. It represents
+ * an atomic abstraction in the design with no behavioral
+ * forks.
+ */
+class SequentialDesignAbstraction {
+    /**
+     * Initializes the sequential design abstraction.
+     */
+    constructor () {
+
+    }
+}
+
+export default SequentialDesignAbstraction;
