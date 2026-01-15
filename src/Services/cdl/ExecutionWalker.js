711977e3ad8c5ea1d31b0639a84a14867e0cc8cd
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-15 13:08:13 -0500

    Add empty execution walked class.

---
+++ src/Services/cdl/ExecutionWalker.js
@@ -0,0 +1,40 @@
+/**
+ * Walks the execution given all the thread files. It is used
+ * by the DAL object to move through the execution using the
+ * design.
+ */
+class ExecutionWalker {
+    /**
+     * Initialize the execution walker with the threads.
+     * @param {Object} threads
+     */
+    constructor (threads) {
+
+    }
+
+    /**
+     * Scans the threads for atomic behaviors to being
+     * walking with the DALSpec.
+     */
+    scanForAtomic () {
+
+    }
+
+    /**
+     * Moves to the next execution in the execution when requested
+     * by the DAL. It does not set the state until DAL verifies
+     * that the design expects this move.
+     */
+    getNext () {
+
+    }
+
+    /**
+     * Tracks an output to the new position and sets the state.
+     */
+    trackOutput () {
+
+    }
+}
+
+export default ExecutionWalker;
