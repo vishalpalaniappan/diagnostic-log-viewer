52c3bae21bfa84e326ac2543027d8e2c31fcbc66
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-15 12:12:55 -0500

    Add empty DAL class.

---
+++ src/Services/cdl/DAL.js
@@ -0,0 +1,14 @@
+/**
+ * Design Abstraction Language class to
+ * work with an instrumented design abstraction.
+ */
+class DAL {
+    /**
+     * Initialize the DAL Interpretter
+     */
+    constructor () {
+
+    }
+}
+
+export default DAL;
