ec0e9bdebe2c38dd63ad058ad31b55e24c946ffd
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-15 15:07:49 -0500

    Remove outdated function.

--- src/Services/cdl/ExecutionWalker.js
+++ src/Services/cdl/ExecutionWalker.js
@@ -43,14 +43,6 @@ class ExecutionWalker {
     }
 
     /**
-     * Scans the threads for atomic behaviors to being
-     * walking with the DALSpec.
-     */
-    scanForAtomic () {
-
-    }
-
-    /**
      * Moves to the next execution in the execution when requested
      * by the DAL. It does not set the state until DAL verifies
      * that the design expects this move.
