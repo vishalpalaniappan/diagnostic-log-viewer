1310511002dd9884c8d02c1f4dad04183523c515
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-17 07:04:37 -0500

    Create abstraction steps and use it to evaluate new behavior.

--- src/Services/cdl/DesignAbstraction.js
+++ src/Services/cdl/DesignAbstraction.js
@@ -1,4 +1,4 @@
-
+import AbstractionStep from "./AbstractionStep";
 /**
  * Represents an instance of an abstraction object
  * loaded intot he abstraction stack.
@@ -11,15 +11,30 @@ class DesignAbstraction {
      */
     constructor(abstraction, step) {
         this.abstraction = abstraction;
-        this.step = step;
+        for (let i = 0; i < this.abstraction.steps.length; i++) {
+            this.abstraction.steps[i] = new AbstractionStep(
+                this.abstraction.steps[i]
+            );
+        }
+        this.step = 0;
     }
+
+    /**
+     * Returns the current step in the design abstraction.
+     * @return {Object}
+     */
+    getCurrentStep () {
+        return this.abstraction.steps[this.step];
+    }
+
     /**
      * Given an ID, if the next step
      * can be taken given the current state.
-     * @param {String} id 
+     * @param {String} id
      */
-    testNext(id) {
-
+    testNext (id) {
+        const currentStep = this.getCurrentStep();
+        currentStep.evaluateBehavior(id);
     }
 }
 
