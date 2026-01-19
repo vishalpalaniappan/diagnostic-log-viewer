20aef02fbde5a6937799986d20a2723ee9261f98
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-19 09:27:32 -0500

    Add selector repeat step.

--- src/Services/cdl/DAL/Steps/SelectorRepeatStep.js
+++ src/Services/cdl/DAL/Steps/SelectorRepeatStep.js
@@ -8,7 +8,56 @@ class SelectorRepeatStep {
      * @param {Object} step
      */
     constructor (step) {
+        this.step = step;
+        this.type = "selector_repeat";
 
+        this.optionTotal = step.options.length;
+        this.optionCount = 0;
+        this.done = false;
+    }
+
+    /**
+     * Evaluates the step given the behavior.
+     * @param {Object} info
+     * @return {Object|null}
+     */
+    evaluateBehavior (info) {
+        const behavior = info.behavioralId;
+        const functional = info.functionalId;
+
+        for (let i = 0; i < this.step.options.length; i++) {
+            const step = this.step.options[i];
+            if (step.module === behavior) {
+                this.done = false;
+                return this.getResponse(STEP.GOTO_MODULE, {module: behavior});
+            }
+        }
+
+        // Inc count to indicate that we visited an option that didn't resolve.
+        this.optionCount++;
+
+        if (this.optionCount >= this.optionTotal) {
+            // If we have visited all the options and it didn't resolve, then
+            // this indicates an error.
+            this.done = true;
+            return this.getResponse(STEP.STEP_DONE, null);
+        } else {
+            // We still have more options to visit, so we should continue.
+            return this.getResponse(STEP.SAME_STEP);
+        }
+    }
+
+    /**
+     * Generates response to the design abstraction.
+     * @param {String} id
+     * @param {Object} args
+     * @return {Object}
+     */
+    getResponse (id, args) {
+        return {
+            "id": id,
+            "args": args,
+        };
     }
 }
 
