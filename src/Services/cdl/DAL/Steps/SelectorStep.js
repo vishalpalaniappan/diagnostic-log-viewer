b3b7b416394f838caa6d21b767a52b280f59bd24
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-20 04:46:57 -0500

    Remove inaccurate doc and implementation.

--- src/Services/cdl/DAL/Steps/SelectorStep.js
+++ src/Services/cdl/DAL/Steps/SelectorStep.js
@@ -12,9 +12,6 @@ class SelectorStep {
         this.step = step;
         this.index = index;
         this.type = "selector";
-
-        this.optionTotal = step.options.length;
-        this.optionCount = 0;
         this.done = false;
     }
 
@@ -27,10 +24,6 @@ class SelectorStep {
         const behavior = info.behavioralId;
         const functional = info.functionalId;
 
-        if (this.done) {
-            return this.getResponse(STEP.STEP_DONE, null);
-        }
-
         // Check if the provided module was selected and go to the module.
         for (let i = 0; i < this.step.options.length; i++) {
             const step = this.step.options[i];
@@ -41,28 +34,8 @@ class SelectorStep {
             }
         }
 
-        /**
-         * In the section below, I am incrementing option count to indicate
-         * that I have visited an option in this selector step. Until all the
-         * options have been visited (if they didn't already resolve), then
-         * we are still in this step. More importantly, if we visit a behavior
-         * that isn't a valid option for this selector, then we have to indicate
-         * that there is a mistake in the instrumentation because we expected
-         * the selectors but the execution didn't reflect that.
-         */
-
-        // Inc count to indicate that we visited an option that didn't resolve.
-        this.optionCount++;
-
-        if (this.optionCount >= this.optionTotal) {
-            // If we have visited all the options and it didn't resolve, then
-            // this indicates an error.
-            this.done = true;
-            return this.getResponse(STEP.STEP_DONE, null);
-        } else {
-            // We still have more options to visit, so we should continue.
-            return this.getResponse(STEP.SAME_STEP);
-        }
+        this.done = true;
+        return this.getResponse(STEP.STEP_DONE, null);
     }
 
     /**
