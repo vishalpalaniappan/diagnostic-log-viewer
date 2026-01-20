d373a5a69d82e654f3904e8e919d4062f6a936b3
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-20 14:49:58 -0500

    Save header to the execution object.

--- src/Services/cdl/DAL/AbstractionStack.js
+++ src/Services/cdl/DAL/AbstractionStack.js
@@ -67,6 +67,7 @@ class AbstractionStack {
          */
         const executionEntry = {...this.initialPosition.execution};
         executionEntry.varStack = thread.getVariablesAtPosition(this.initialPosition.position);
+        executionEntry.header = thread.header.logTypeMap[executionEntry.value];
 
         const status = this.evaluateBehavior(
             executionEntry,
@@ -110,6 +111,7 @@ class AbstractionStack {
 
             const executionEntry = {...entry};
             executionEntry.varStack = thread.getVariablesAtPosition(position);
+            executionEntry.header = thread.header.logTypeMap[executionEntry.value];
 
             // Evaluate the execution position using the design.
             const status = this.evaluateBehavior(
