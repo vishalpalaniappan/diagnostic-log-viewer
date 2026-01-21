7d48e35af40caba303f23ee7b0559e2972f2fcac
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-21 15:44:12 -0500

    Load atomic behavior in design diagram component.

--- src/Components/DesignDiagram/DesignDiagram.js
+++ src/Components/DesignDiagram/DesignDiagram.js
@@ -1,14 +1,16 @@
-import React, {useCallback, useRef, useState} from "react";
+import React, {useCallback, useContext, useEffect, useState} from "react";
 
 import {
-    Controls,
     Background,
+    Controls,
     ReactFlow,
     ReactFlowProvider,
     useEdgesState,
     useNodesState,
     useReactFlow
 } from "@xyflow/react";
+
+import BehaviorContext from "../../Providers/BehaviorContext";
 
 import "@xyflow/react/dist/style.css";
 
@@ -25,8 +27,16 @@ import "./DesignDiagram.scss";
  * @return {JSX.Element}
  */
 export function DesignDiagram ({}) {
+    const {activeBehavior, behavior} = useContext(BehaviorContext);
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+    useEffect(() => {
+        if (behavior && activeBehavior) {
+            const entry = behavior.atomicAbstractions[activeBehavior.atomicUid];
+            console.log("Selected Atomic Abstraction:", entry);
+        }
+    }, [behavior, activeBehavior]);
 
     return (
         <ReactFlow
