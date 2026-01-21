2b1a6c97cc16eb7846a947ba3ab0692d5b624aff
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-21 15:12:31 -0500

    Add design diagram component.

---
+++ src/Components/DesignDiagram/DesignDiagram.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useRef} from "react";
+
+import "./DesignDiagram.scss";
+
+/**
+ * @return {JSX.Element}
+ */
+export function DesignDiagram ({}) {
+    return (
+        <div className="w-100 h-100 design-diagram-container">
+
+        </div>
+    );
+}
