fe0a51560bb90eb00fa6623a258c1fc9aa7f0b98
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-24 14:48:12 -0500

    Revert "Change incorrect word."
    This reverts commit 28182ab29831d47451392f9c5567c16f8e07effe.

--- src/Services/cdl/DAL/SemanticOutput/Abstraction.js
+++ src/Services/cdl/DAL/SemanticOutput/Abstraction.js
@@ -20,7 +20,7 @@ class Abstraction {
     }
 
     /**
-     * Evaluate the state variables of the abstraction.
+     * Evaluate the state variables of the behavior.
      */
     evaluateState () {
         if (!("state_variables" in this.abstractionInfo)) {
