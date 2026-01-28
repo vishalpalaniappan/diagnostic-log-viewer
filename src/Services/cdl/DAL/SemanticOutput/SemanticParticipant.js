d215259a60e51ce2d33ad0594446668f8455eef5
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-28 07:50:21 -0500

    Add semantic participant class.

---
+++ src/Services/cdl/DAL/SemanticOutput/SemanticParticipant.js
@@ -0,0 +1,13 @@
+/**
+ * This class contains a semantic participant.
+ */
+class SemanticParticipant {
+    /**
+     * Initialize the semantic participant.
+     */
+    constructor () {
+
+    }
+}
+
+export default SemanticParticipant;
