1063bc46af3c23afecdd3d6ccb45e801410bf2e6
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-28 08:41:35 -0500

    Add placeholder class.

---
+++ src/Services/cdl/DAL/SemanticOutput/PlaceHolder.js
@@ -0,0 +1,20 @@
+/**
+ * This class contains a placeholder for a design abstraction.
+ * These placeholders are used in the semantic abstraction action sentences.
+ * They can access the value of semantic participants directly, they can
+ * process the vaulue of semantic participants, or they can access
+ * the state of a seleted abstraction in one of the steps.
+ *
+ * Through this, the semantic abstraction can generate meaningful
+ * action sentences that reflect the actual execution of the design.
+ */
+class PlaceHolder {
+    /**
+     * Initialize the placeholder.
+     */
+    constructor () {
+
+    }
+}
+
+export default PlaceHolder;
