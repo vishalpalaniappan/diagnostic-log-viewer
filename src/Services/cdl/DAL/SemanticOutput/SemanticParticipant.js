eff46c5a5cb554ec22e4a7ff3027e60ce31f91c1
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-28 09:07:06 -0500

    Add doc for semantic participants.

--- src/Services/cdl/DAL/SemanticOutput/SemanticParticipant.js
+++ src/Services/cdl/DAL/SemanticOutput/SemanticParticipant.js
@@ -1,5 +1,16 @@
 /**
  * This class contains a semantic participant.
+ *
+ * The semantic participant represents an entity
+ * involved in the semantic abstraction. It is defined
+ * at the behavioral level and can hold the properties
+ * of the participants in the behavior.
+ * 
+ * For example, in a behavior that is taking a book
+ * from the basket, the semantic participants are the
+ * basket and the book. This defines the behavior
+ * uniquely and allows the semantic abstraction to
+ * refer to these participants in its action sentences.
  */
 class SemanticParticipant {
     /**
