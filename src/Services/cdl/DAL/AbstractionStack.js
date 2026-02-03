508b60997f1ce033a6d6e9a72b05d01d4cfad8f8
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-03 15:37:15 -0500

    Process active abstraction even if it doesn't end in the trace (failure or termination).

--- src/Services/cdl/DAL/AbstractionStack.js
+++ src/Services/cdl/DAL/AbstractionStack.js
@@ -162,6 +162,14 @@ class AbstractionStack {
                 }
             }
         }
+
+        // End the active abstraction even if it doesn't end through the
+        // transform. This could happen because of failure or termination.
+        if (SemanticTrace && SemanticTrace.activeAbstraction && this.isFork) {
+            SemanticTrace.endForkActiveAbstraction();
+        } else if (SemanticTrace && SemanticTrace.activeAbstraction) {
+            SemanticTrace.endActiveAtomicAbstraction();
+        }
     }
 
     /**
