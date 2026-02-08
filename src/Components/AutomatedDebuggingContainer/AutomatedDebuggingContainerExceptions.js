b298783541fee47e54b238b04ce8199a35a51bca
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-08 13:16:52 -0500

    Add text to indicate that the root cause was not observed.

--- src/Components/AutomatedDebuggingContainer/AutomatedDebuggingContainerExceptions.js
+++ src/Components/AutomatedDebuggingContainer/AutomatedDebuggingContainerExceptions.js
@@ -39,6 +39,19 @@ export function AutomatedDebuggingContainerExceptions ({}) {
                 exceptionsList.push(
                     <DebuggerNodeRootCause key={i + "r"} node={node} />
                 );
+            } else {
+                // I'm simulating an invariant node an adding the failed
+                // root cause msg. It a bit hacky but it works for now.
+                // TODO: Create a dedicated node for this?
+                const node = {};
+                node.level = 0;
+                node.violation = {};
+                node.violation.violation_type = "invariant";
+                // eslint-disable-next-line max-len
+                node.text = "The root cause of this failure was not observed and the instrumentation needs to improve to include the semantic invariant which guards this failure modality.";
+                exceptionsList.push(
+                    <DebuggerNodeRootCause key={i + "r"} node={node} />
+                );
             }
         }
         setRows(exceptionsList);
